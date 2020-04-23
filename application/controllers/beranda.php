c9cee29b3f9a06da09c5fb0941a3b33faf1468d2
saskiapa <62656427+saskiapa@users.noreply.github.com>
2020-04-23 16:53:53 +0700

    Menyelesaikan input pemeriksaan

--- application/controllers/beranda.php
+++ application/controllers/beranda.php
@@ -21,6 +21,10 @@ class beranda extends CI_Controller {
                 'nLayanan' => $this->layananModel->getBanyakLayanan(),
                 'dataPemeriksaan' => json_decode($this->pemeriksaanModel->getPemeriksaan())
             );
+            $error = $this->session->flashdata('error');
+            if(isset($error)){
+                $data['error_msg'] = 'Belum ada dokter atau pasien atau layanan';
+             }
             $this->load->view('beranda',$data);
         }
         else {
@@ -29,9 +33,42 @@ class beranda extends CI_Controller {
     }
     
     public function inputPemeriksaan() {
-        $this->load->view('inputPemeriksaan');
+        if ($this->session->has_userdata('username')){
+            if($this->pemeriksaanModel->isExist()){
+               $data['dataDokter']=$this->dokterModel->getListDokter();
+               $data['dataPasien']=$this->pasienModel->getListPasien();
+               $data['dataLayanan']=$this->layananModel->getListLayanan();
+               $this->load->view('inputPemeriksaan',$data);
+            }
+            else{
+                $this->session->set_flashdata('error', true);
+                redirect('beranda');
+            }
+
+        }
+        else{
+             redirect('login');
+        }
+       
         if (isset($_POST['btnInputPemeriksaan'])) {
+            //substr("12312 - lala",0,strpos("12312 - lala"," ")+1)
+            $idDokter = substr($this->input->post('dokter'),0,strpos($this->input->post('dokter'),' ')+1);
+            $idPasien = substr($this->input->post('pasien'),0,strpos($this->input->post('pasien'),' ')+1);
+            $idLayanan = substr($this->input->post('layanan'),0,strpos($this->input->post('layanan'),' ')+1);
+            $tanggal=$this->input->post('tanggal');
+            $jam = substr($this->input->post('jam'),0,strpos($this->input->post('jam'),':'));
+            $menit = substr($this->input->post('jam'),strpos($this->input->post('jam'),':')+1,strlen($this->input->post('jam')));
+            $data = array(
+                'idDokter'=> $idDokter,
+                'idPasien'=> $idPasien,
+                'idLayanan'=> $idLayanan,
+                'tanggal'=> $tanggal,
+                'jam'=> $jam,
+                'menit'=> $menit
+            );
+            $this->pemeriksaanModel->tambahPemeriksaan($data);
             redirect('beranda/index');
+
         }
     }
 }
